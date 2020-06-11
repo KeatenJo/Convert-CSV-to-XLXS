bc730a1ae2c32f69cfdcc4372c0abbc78c93b553
Keaten Jones <Keatenjones16@gmail.com>
2020-06-10 23:33:15 -0600

    Working Program. Implemented a csv conversion to xlxs. Can convert mutiple files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+
+namespace csvConvertxls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    Convert(arg);
+                }
+            } else
+            {
+                Console.WriteLine("Please Supply the name of your csv file.");
+                Console.WriteLine("Ex.  ./csvConvertxls foo.txt bar.txt");
+            }
+            
+        }
+
+
+        public static void Convert(string name)
+        {
+            string csvFileName = name;
+            string[] temp_name = name.Split(".");
+            string excelFileName = temp_name[0] + ".xlsx";
+
+            string worksheetsName = temp_name[0];
+
+            bool firstRowIsHeader = false;
+
+            var format = new ExcelTextFormat();
+            format.Delimiter = ',';
+            format.EOL = "\r";              // DEFAULT IS "\r\n";
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFileName)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(worksheetsName);
+                worksheet.Cells["A1"].LoadFromText(new FileInfo(csvFileName), format, OfficeOpenXml.Table.TableStyles.Medium27, firstRowIsHeader);
+                package.Save();
+            }
+
+            Console.WriteLine("Finished converting " + csvFileName + " to " + excelFileName + ".");
+        }
+    }
+}
