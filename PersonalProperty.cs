a032e90b36dce5906d28bc2c95e8c3dd6a4e4cea
Keaten Jones <keatenjones16@gmail.com>
2020-06-11 00:06:51 -0600

    packaging up exes

---
+++ PersonalProperty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csvConvertxls
+{
+    public class PersonalProperty
+    {
+
+        public string Account { get; set; }
+        public string OwnerName { get; set; }
+        public string YearAquired { get; set; }
+        public string Description { get; set; }
+        public double OrgCost { get; set; }
+        public string PropertyCode { get; set; }
+    }
+}
+//Account,OwnerName,YearAquired,Description,OrgCost,PropertyCode
